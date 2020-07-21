ae85ec43b384de907a74e1f9cf288b7fa67e4bb6
Matias Collado <matias.collado@alumnos.frgp.utn.edu.ar>
2020-07-21 03:51:25 -0300

    Revert "Revert "Cambios""
    This reverts commit eb9183ecb5cec1487bd88f2b70ed2897c256e1ae.

--- Cacho 4.0/Proyecto Cacho 4.0/Menues.h
+++ Cacho 4.0/Proyecto Cacho 4.0/Menues.h
@@ -8,15 +8,66 @@ int MenuPrincipal(){
     cout<<"3- SUSCRIPTORES"<<endl;
     cout<<"4- PERSONAL"<<endl;
     cout<<"5- CONFIGURACIONES"<<endl;
-    cout<<"6- MENU INFORMES"<<endl<<endl;
+    cout<<"6- MENU INFORMES"<<endl;
+    cout<<"0- SALIR"<<endl<<endl;
+    cout<<"OPCION: ";
     cin>>opcion;
+    system("cls");
     return opcion;
 }
 
-int MenuCandy(){
+void MenuGestionCandy(){
     int opcion;
-    cin>>opcion;
-    return opcion;
+    while(true){
+        cout<<"1- AGREGAR PRODUCTO"<<endl;
+        cout<<"2- MODIFICAR PRODUCTO"<<endl;
+        cout<<"3- ELIMINAR PRODUCTO"<<endl;
+        cout<<"4- LISTAR PRODUCTOS"<<endl;
+        cout<<"0- VOLVER"<<endl<<endl;
+        cout<<"OPCION: ";
+        cin>>opcion;
+        system("cls");
+        switch(opcion){
+        case 1:
+            AgregarCandy();
+            break;
+        case 2:
+            ElegirProductoCandy();
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 0:
+            return;
+            break;
+        }
+    }
+}
+void MenuCandy(){
+    int opcion;
+    while(true){
+        cout<<"1- AGREGAR VENTA"<<endl;
+        cout<<"2- GESTION CANDY"<<endl;
+        cout<<"0- VOLVER"<<endl<<endl;
+        cout<<"OPCION: ";
+        cin>>opcion;
+        system("cls");
+        switch(opcion){
+        case 1:
+            break;
+        case 2:
+            MenuGestionCandy();
+            break;
+        case 0:
+            return;
+            break;
+        default:
+            cout<<"OPCION INVALIDA"<<endl;
+            break;
+        }
+    }
+    return;
 }
 
 #endif // MENUES_H_INCLUDED
