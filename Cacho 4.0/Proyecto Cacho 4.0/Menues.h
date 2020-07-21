a3a26bfd1aa0ecdf587d7729f01891813a461b4d
Matias Collado <matias.collado@alumnos.frgp.utn.edu.ar>
2020-07-21 01:24:22 -0300

    Prueba

---
+++ Cacho 4.0/Proyecto Cacho 4.0/Menues.h
@@ -0,0 +1,8 @@
+#ifndef MENUES_H_INCLUDED
+#define MENUES_H_INCLUDED
+
+void MenuPrincipal(){
+
+}
+
+#endif // MENUES_H_INCLUDED
---
+++ Cacho 4.0/Proyecto Cacho 4.0/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Menues.h"
+
+using namespace std;
+
+int main()
+{
+    switch(MenuPrincipal()){
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        break;
+    case 6:
+        break;
+    }
+    return 0;
+}
