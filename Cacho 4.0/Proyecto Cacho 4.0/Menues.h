ed63cb767f0801ded8708b73d33e43479c315c80
MartinaP16 <54824480+MartinaP16@users.noreply.github.com>
2020-07-21 03:31:50 -0300

    MenuSucriptores bb

--- Cacho 4.0/Proyecto Cacho 4.0/Menues.h
+++ Cacho 4.0/Proyecto Cacho 4.0/Menues.h
@@ -1,6 +1,8 @@
 #ifndef MENUES_H_INCLUDED
 #define MENUES_H_INCLUDED
 
+#include "Suscriptores.h"
+
 int MenuPrincipal(){
     int opcion;
     cout<<"1- MENU DEL CANDY"<<endl;
@@ -12,5 +14,58 @@ int MenuPrincipal(){
     cin>>opcion;
     return opcion;
 }
+
+void MenuSuscriptores(){
+
+int Opcion;
+
+cout << "1- NUEVO SUSCRIPTOR" << endl;
+cout << "2- ELIMINAR SUSCRIPTOR" << endl;
+cout << "3- LISTAR SUSCRIPTORES" << endl;
+cout << "4- CONSULTAR SUSCRIPTOR" << endl;
+cout << "5- GESTIONAR CANJES DE SUSCRIPTORES" << endl;
+cout << "6- GESTIONAR BENEFICIOS" << endl;
+
+cin >> Opcion;
+
+switch(Opcion){
+
+case 1:
+    {Cliente reg;
+    if(reg.Cargar()){
+        reg.Grabar();
+    }}
+    break;
+
+case 2:
+    {
+        system("cls");
+        int dni;
+
+    cout << "INGRESE NUMERO DE DNI: ";
+    cin >> dni;
+
+    if(EliminarSuscriptor(dni)){
+        cout << "Suscriptor eliminado"<< endl;
+    }
+    else {cout << "No se pudo eliminar suscriptor" << endl;}
+    }
+    break;
+
+case 3:
+    break;
+
+case 4:
+    break;
+
+case 5:
+    break;
+
+case 6:
+    break;
+}
+
+}
+
 
 #endif // MENUES_H_INCLUDED
--- Cacho 4.0/Proyecto Cacho 4.0/main.cpp
+++ Cacho 4.0/Proyecto Cacho 4.0/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
-
+#include <string.h>
+#include "Fecha.h"
 using namespace std;
 
+class Persona{
+protected:
+	char Nombre[50],Apellido[50],Genero;
+	Fecha Nacimiento;
+public:
+	char * GetNombre(){return Nombre;}
+	char * GetApellido(){return Apellido;}
+	char GetGenero(){return Genero;}
+	Fecha GetNacimiento(){return Nacimiento;}
+	void SetNombre(char* x){strcpy(Nombre,x);}
+	void SetApellido(char* x){strcpy(Apellido,x);}
+	void SetGenero(char x){Genero=x;}
+	void SetNacimiento(Fecha x){Nacimiento=x;}
+};
+#include "Suscriptores.h"
 #include "Menues.h"
 
 int main()
@@ -13,6 +29,8 @@ int main()
         case 2:
             break;
         case 3:
+            system("cls");
+            MenuSuscriptores();
             break;
         case 4:
             break;
